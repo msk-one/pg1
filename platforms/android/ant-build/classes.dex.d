fbdcdd0dd1e36c319368dec21478194691431b31
Msk <msk.sk.one@gmail.com>
2014-12-10 16:32:09 +0100

    Added bootstrap

--- platforms/android/ant-build/classes.dex.d
+++ platforms/android/ant-build/classes.dex.d
@@ -6,4 +6,11 @@
 /home/msk/pgproj/platforms/android/ant-build/classes/io/cordova/hellocordova/CordovaApp.class \
 /home/msk/pgproj/platforms/android/ant-build/classes/io/cordova/hellocordova/R$string.class \
 /home/msk/pgproj/platforms/android/ant-build/classes/io/cordova/hellocordova/R.class \
+/home/msk/pgproj/platforms/android/ant-build/classes/io/cordova/ufogram/BuildConfig.class \
+/home/msk/pgproj/platforms/android/ant-build/classes/io/cordova/ufogram/R$attr.class \
+/home/msk/pgproj/platforms/android/ant-build/classes/io/cordova/ufogram/R$xml.class \
+/home/msk/pgproj/platforms/android/ant-build/classes/io/cordova/ufogram/R$drawable.class \
+/home/msk/pgproj/platforms/android/ant-build/classes/io/cordova/ufogram/CordovaApp.class \
+/home/msk/pgproj/platforms/android/ant-build/classes/io/cordova/ufogram/R$string.class \
+/home/msk/pgproj/platforms/android/ant-build/classes/io/cordova/ufogram/R.class \
 /home/msk/pgproj/platforms/android/ant-build/dexedLibs/classes-090833fe9f760d2578377cc00e46b7aa.jar \
